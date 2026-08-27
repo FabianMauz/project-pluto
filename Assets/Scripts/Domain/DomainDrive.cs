52a0dcf683e018527f165717746e8a888283c1a7
Fabian Mauz <fabian66@freenet.de>
2026-08-27 23:25:13 +0200

    feat: add domain drive

--- Assets/Scripts/Domain/DomainDrive.cs
+++ Assets/Scripts/Domain/DomainDrive.cs
@@ -1,3 +1,35 @@
-public class DomainDrive{
-  
+public class DomainDrive {
+
+    public int levelOfSpeed { private set; get; }
+    public int levelOfEvade { private set; get; }
+    public float getEvadeChance() {
+        return (levelOfEvade - 1) * 0.05f;
+    }
+
+    public DomainDrive(int startSpeed, int startEvade) {
+        levelOfEvade = startEvade;
+        levelOfSpeed = startSpeed;
+
+    }
+
+    public float getSpeed() {
+        return 3 + .25f * levelOfSpeed;
+    }
+
+    public void upgradeSpeed() {
+        levelOfSpeed++;
+    }
+
+    public void upgradeEvade() {
+        levelOfEvade++;
+    }
+
+    public bool isEvadeUpgradable() {
+        return levelOfEvade <= 5;
+    }
+    public bool isSpeedUpgradable() {
+        return levelOfSpeed <= 5;
+    }
+
+
 }
--- Assets/Scripts/Domain/DomainShip.cs
+++ Assets/Scripts/Domain/DomainShip.cs
@@ -1,7 +1,11 @@
+using System;
+
 public class DomainShip {
     public DomainMainCannon mainCanon { get; private set; }
+    public DomainDrive drive { get; private set; }
 
     public DomainShip() {
         mainCanon = new DomainMainCannon();
+        drive = new DomainDrive(1, 1);
     }
 }
