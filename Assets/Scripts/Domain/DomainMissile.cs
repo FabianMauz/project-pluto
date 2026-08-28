66453709abee414fbc80d3a06fd55f926a548701
Fabian Mauz <fabian66@freenet.de>
2026-08-28 23:07:40 +0200

    feat: add domain missile and introduce domain stats

--- Assets/Scripts/Domain/DomainMissile.cs
+++ Assets/Scripts/Domain/DomainMissile.cs
@@ -1,3 +1,74 @@
 public class DomainMissile {
-   
+    public int levelOfDamage;
+    public int levelOfAmount;
+    public int levelOfArmor;
+    public int levelOfRange;
+    public int levelOfReloadSpeed;
+
+    public DomainMissile(int startDamage, int startAmount, int startArmor, int startRange, int startReloadSpeed) {
+        levelOfAmount = startAmount;
+        levelOfArmor = startArmor;
+        levelOfDamage = startDamage;
+        levelOfRange = startRange;
+        levelOfReloadSpeed = startReloadSpeed;
+    }
+
+    public void upgradeReloadSpeed() {
+        levelOfReloadSpeed++;
+    }
+
+    public void upgradeDamage() {
+        levelOfDamage++;
+    }
+
+    public void upgradeArmor() {
+        levelOfArmor++;
+    }
+
+    public void upgradeRange() {
+        levelOfRange++;
+    }
+
+    public void upgradeAmount() {
+        levelOfAmount++;
+    }
+
+    public bool isReloadSpeedUpgradable() {
+        return levelOfReloadSpeed <= DomainStats.missileReloadSpeed.Length - 1;
+    }
+    public bool isDamageUpgradable() {
+        return levelOfDamage <= DomainStats.missileDamage.Length - 1;
+    }
+
+    public bool isRangeUpgradable() {
+        return levelOfRange <= DomainStats.missileRange.Length - 1;
+    }
+
+    public bool isArmorUpgradable() {
+        return levelOfArmor <= DomainStats.missileArmor.Length - 1;
+    }
+
+    public bool isAmountUpgradable() {
+        return levelOfAmount <= DomainStats.MAX_MISSILE_AMOUNT - 1;
+    }
+
+    public float getDamage() {
+        return DomainStats.missileDamage[levelOfDamage - 1];
+    }
+
+    public float getRange() {
+        return DomainStats.missileRange[levelOfRange - 1];
+    }
+
+    public float getReloadSpeed() {
+        return DomainStats.missileReloadSpeed[levelOfReloadSpeed - 1];
+    }
+
+    public float getArmor() {
+        return DomainStats.missileArmor[levelOfArmor - 1];
+    }
+
+    public float getAmount() {
+        return levelOfAmount;
+    }
 }
---
+++ Assets/Scripts/Domain/DomainStats.cs
@@ -0,0 +1,8 @@
+public abstract class DomainStats {
+    public static float[] missileDamage = new float[] { 0, 0, 0, 0, 0 };
+    public static float[] missileRange = new float[] { 0, 0, 0, 0, 0 };
+    public static float[] missileReloadSpeed = new float[] { 0, 0, 0, 0, 0 };
+    public static float[] missileArmor = new float[] { 0, 0, 0, 0, 0 };
+
+    public static int MAX_MISSILE_AMOUNT = 3;
+}
