6a008d62c4a3b68967de33cf75ed96484563f4d7
Fabian Mauz <fabian66@freenet.de>
2026-08-30 10:15:53 +0200

    feat: add domain defence weapon

--- Assets/Scripts/Domain/DomainDefenceWeapon.cs
+++ Assets/Scripts/Domain/DomainDefenceWeapon.cs
@@ -1,3 +1,52 @@
 public class DomainDefenceWeapon {
+    public int amount { get; private set; }
+    public int levelOfDamage { get; private set; }
+    public int levelOfRange { get; private set; }
+    public int levelOfReloadSpeed { get; private set; }
 
+    //Damage
+    public void upgradeDamage() {
+        levelOfDamage++;
+    }
+
+    public bool isDamageUpgradable() {
+        return levelOfDamage <= DomainStats.defenceCannonDamage.Length - 1;
+    }
+
+    public float getDamage() {
+        return DomainStats.defenceCannonDamage[levelOfDamage - 1];
+    }
+    //Range
+    public void upgradeRange() {
+        levelOfRange++;
+    }
+
+    public bool isRangeUpgradable() {
+        return levelOfRange <= DomainStats.defenceCannonRange.Length - 1;
+    }
+
+    public float getRange() {
+        return DomainStats.defenceCannonRange[levelOfRange - 1];
+    }
+    //ReloadSpeed
+    public void upgradeReloadSpeed() {
+        levelOfReloadSpeed++;
+    }
+
+    public bool isReloadSpeedUpgradable() {
+        return levelOfReloadSpeed <= DomainStats.defenceCannonReloadSpeed.Length - 1;
+    }
+
+    public float getReloadSpeed() {
+        return DomainStats.defenceCannonReloadSpeed[levelOfReloadSpeed - 1];
+    }
+
+    //Amount
+    public void upgradeAmount() {
+        amount++;
+    }
+
+    public bool isAmountUpgradable() {
+        return amount < 2;
+    }
 }
